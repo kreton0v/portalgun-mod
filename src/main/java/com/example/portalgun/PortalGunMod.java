eb96d6dad6f6b10b7ba843311795787386f9cec7
kreton0v <pgarry900@gmail.com>
2026-08-20 14:05:55 +0300

    Update PortalGunMod.java

--- src/main/java/com/example/portalgun/PortalGunMod.java
+++ src/main/java/com/example/portalgun/PortalGunMod.java
@@ -1,8 +1,6 @@
 package com.example.portalgun;
 
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,7 +8,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-@Mod("portalgun")
+@Mod(PortalGunMod.MOD_ID)
 public class PortalGunMod {
     public static final String MOD_ID = "portalgun";
 
@@ -18,18 +16,10 @@ public class PortalGunMod {
         DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
     public static final RegistryObject<Item> PORTAL_GUN = ITEMS.register("portal_gun",
-        () -> new Item(new Item.Properties().stacksTo(1)));
+        () -> new PortalGunItem(new Item.Properties().stacksTo(1)));
 
     public PortalGunMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         ITEMS.register(modEventBus);
-        modEventBus.addListener(this::addCreative);
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(PORTAL_GUN);
-        }
     }
 }
